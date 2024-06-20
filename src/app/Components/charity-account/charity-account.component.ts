b0543e5bded2dc1d53b0150b5dc697ad84918fdd
Reem <reem.osama3052001@gmail.com>
2024-06-21 00:21:47 +0300

    fix upload img

--- src/app/Components/charity-account/charity-account.component.ts
+++ src/app/Components/charity-account/charity-account.component.ts
@@ -8,68 +8,75 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-charity-account',
   standalone: true,
-  imports: [ReactiveFormsModule,CommonModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './charity-account.component.html',
-  styleUrl: './charity-account.component.css'
+  styleUrls: ['./charity-account.component.css']  // Changed to styleUrls
 })
 export class CharityAccountComponent {
 
- 
-    selectedFile: any;
-    charityData = {  // Change to charityData
-      name: '',
-      description: '',
-      websiteUrl: '',
-      imgUrl: '',
-    };
-    isLoading = false;
-    msgError = '';
-  
-    constructor(private _CharityService: CharityService,  // Use CharityService
-                private _Router: Router,
-                private authservice: AuthService) {}
-  
-    ngOnInit() {
-      const userId = this.authservice.getUserId();
-      console.log(userId);
-    
-    }
-  
-    charityForm: FormGroup = new FormGroup({
-      name: new FormControl('', [Validators.required]),
-      description: new FormControl('', [Validators.required]),
-      websiteUrl: new FormControl('', [Validators.required]),
-      imgUrl: new FormControl('', [Validators.required]),
+  id: string = "";
+  selectedFile: File | null = null;  // Changed type to File | null
+  charityData = {
+    name: '',
+    description: '',
+    websiteUrl: '',
+    imgUrl: '',
+  };
+  isLoading = false;
+  msgError = '';
+
+  constructor(private _CharityService: CharityService,
+              private _Router: Router,
+              private authservice: AuthService) { }
+
+  ngOnInit() {
+    this._CharityService.getAccountID("sero").subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err) => {
+        console.log(err.error);
+      }
     });
-  
-    onFileChange(event: any): void {
-      if (event.target.files.length > 0) {
-        this.selectedFile = event.target.files[0];
-        this.charityForm.patchValue({ imgUrl: this.selectedFile.name });
+  }
+
+  charityForm: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    description: new FormControl('', [Validators.required]),
+    websiteUrl: new FormControl('', [Validators.required]),
+    imgUrl: new FormControl('', [Validators.required]),
+  });
+
+  onFileChange(event: any): void {
+    if (event.target.files.length > 0) {
+      this.selectedFile = event.target.files[0];
+      // Check if selectedFile is not null before accessing its name property
+      if (this.selectedFile) {
+        this.charityData.imgUrl = this.selectedFile.name;
       }
     }
-  
+  }
 
-  
-    onSubmit() {
-      if (this.charityForm.valid && this.selectedFile) {
-        const charityData = {
-          name: this.charityForm.get('name')?.value,
-          description: this.charityForm.get('description')?.value,
-          websiteUrl: this.charityForm.get('websiteUrl')?.value,
-          imgUrl: this.selectedFile.name,
-          applicationUserId:this._CharityService.getAccountID(this.charityForm.get('name')?.value)
+  onSubmit() {
+    if (this.charityForm.valid && this.selectedFile) {
+      const charityData = {
+        name: this.charityForm.get('name')?.value,
+        description: this.charityForm.get('description')?.value,
+        websiteUrl: this.charityForm.get('websiteUrl')?.value,
+        imgUrl: this.selectedFile.name
+      };
 
-        };
-  
-        console.log("the charity form data", charityData);
-  
-        this._CharityService.addcharity(charityData).subscribe({  // Use CharityService method
-          next: (response) => {
-            console.log(response.message);
-            this._Router.navigate([`/charity/${response.message.charityId}`]);  // Assuming route structure
-          },
-          
-        });
-      }}
+      console.log("the charity form data", charityData);
+
+      this._CharityService.addcharity(charityData).subscribe({
+        next: (response) => {
+          console.log(response.message);
+          this._Router.navigate([`/charity/${response.message.charityId}`]);
+        },
+        error: (err) => {
+          console.error(err);
+        }
+      });
     }
+  }
+}
