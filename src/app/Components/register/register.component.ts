53e399faceb8d602843888c6e73e134d32746f23
Reem <reem.osama3052001@gmail.com>
2024-06-21 20:42:56 +0300

    finish donor register

--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -121,7 +121,7 @@ export class RegisterComponent {
           next: (response) => {
             this.isLoading = false;
             if (response.message == 'success') {
-              this._Router.navigate(['/donorAccount']);
+              this._Router.navigate(['/donoraccount']);
             }
           },
           error: (err: HttpErrorResponse) => {
