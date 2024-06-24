0ee1072c01c48725eea29170a5a6370905c7edcd
Reem <reem.osama3052001@gmail.com>
2024-06-25 00:21:41 +0300

    start working on donor profile

--- src/app/Services/donorService/donor.service.ts
+++ src/app/Services/donorService/donor.service.ts
@@ -31,4 +31,8 @@ getDonorID(id:string)
     return this._httpclient.get<string>(`https://localhost:44377/api/Donor/GetDonorDetails/${userId}`);
   }
 
+  getdonorByID(id:number):Observable<any>{
+    return this._httpclient.get(`https://localhost:44377/api/Donor/${id}`);
+  }
+
 }
