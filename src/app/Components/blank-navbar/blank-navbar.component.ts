f599bb68bb440abf4c7c210bdc73351cb9f6bb06
sarah-sameh <zarasamy18@gmail.com>
2024-06-27 02:56:00 +0300

    router link [profile] in navebar for donor only

--- src/app/Components/blank-navbar/blank-navbar.component.ts
+++ src/app/Components/blank-navbar/blank-navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../Services/auth.service';
 import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { DonorService } from '../../Services/donorService/donor.service';
 
 @Component({
     selector: 'app-blank-navbar',
@@ -12,12 +13,33 @@ import { CommonModule } from '@angular/common';
 })
 export class BlankNavbarComponent implements OnInit {
   accountType: string | null = null;
+  accountId: number | null = null;
 
-  constructor(private _AuthService:AuthService){}
+
+  constructor(private _AuthService:AuthService, private donorService:DonorService){}
 
   ngOnInit() {
     this.accountType = this._AuthService.getUserAccountType();
     console.log('accccount type is', this.accountType);
+
+    const userId = this._AuthService.getUserId();
+    console.log("user id in string is => ",userId);
+
+    if(userId){
+      this.donorService.getDonorID(userId).subscribe({
+        next: (response) => {
+          console.log('response is =>>',response);
+          this.accountId = response
+         
+        },
+        error: (err) => {
+          console.error('Error fetching projects:', err);
+        }
+      
+      })
+
+      }
+
 
   }
   logOutUser():void{
